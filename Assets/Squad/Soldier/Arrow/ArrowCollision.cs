7b1c1eff4395ed6bf431eb970093ac7eb1ad8f6c
Andreas Wilcox <UnknownBum@gmail.com>
2015-06-10 20:52:33 +0200

    Started work on arrows doing damage

--- Assets/Squad/Soldier/Arrow/ArrowCollision.cs
+++ Assets/Squad/Soldier/Arrow/ArrowCollision.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 using System.Linq;
 
-public class ArrowStickOnHit : MonoBehaviour
+public class ArrowCollision : MonoBehaviour
 {
     private bool _isStuck;
+    private float _aliveTime;
+    private const float TimeUntilActive = 1f;
+
+    public void Start()
+    {
+        _aliveTime = 0;
+    }
+
+    public void Update()
+    {
+        _aliveTime += Time.deltaTime;
+    }
 
     void OnCollisionEnter(Collision collision)
     {
         var otherCollider = collision.contacts.First().otherCollider;
-        if(!_isStuck && !otherCollider.gameObject.name.Contains("Arrow"))
+        if(_aliveTime > TimeUntilActive && !_isStuck && !otherCollider.gameObject.name.Contains("Arrow"))
         {
             var rigidBody = GetComponent<Rigidbody>();
             rigidBody.isKinematic = true;
