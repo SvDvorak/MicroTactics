85df72a426ff09a501ee6310a35cbb7077ac8e88
Andreas Wilcox <UnknownBum@gmail.com>
2015-07-03 08:33:08 +0200

    Added camera panning

---
+++ Assets/Camera/PanControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PanControls : MonoBehaviour
+{
+    public AnimationCurve MouseCurve;
+    public float PanSpeed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var panX = MouseCurve.Evaluate(Input.mousePosition.x / Screen.width);
+        var panY = MouseCurve.Evaluate(Input.mousePosition.y / Screen.height);
+
+        var pan = Vector3.ClampMagnitude(new Vector3(panX, 0, panY), 1);
+        transform.Translate(pan * PanSpeed * Time.deltaTime, Space.World);
+    }
+}
