0000c95ca801d20b41be2d8fe697294bd6ce7fd6
Andreas Wilcox <UnknownBum@gmail.com>
2015-07-04 19:45:20 +0200

    Added WASD key camera panning

--- Assets/Camera/PanControls.cs
+++ Assets/Camera/PanControls.cs
@@ -3,18 +3,30 @@
 public class PanControls : MonoBehaviour
 {
     public AnimationCurve MouseCurve;
-    public float PanSpeed;
-
-    void Start()
-    {
-    }
+    public float MousePanSpeed;
+    public float KeysPanSpeed;
 
     void Update()
     {
-        var panX = MouseCurve.Evaluate(Input.mousePosition.x / Screen.width);
-        var panY = MouseCurve.Evaluate(Input.mousePosition.y / Screen.height);
+        transform.Translate((GetMouseMove() + GetKeysMove()) * Time.deltaTime, Space.World);
+    }
 
-        var pan = Vector3.ClampMagnitude(new Vector3(panX, 0, panY), 1);
-        transform.Translate(pan * PanSpeed * Time.deltaTime, Space.World);
+    private Vector3 GetMouseMove()
+    {
+        var panX = MouseCurve.Evaluate(Input.mousePosition.x/Screen.width);
+        var panY = MouseCurve.Evaluate(Input.mousePosition.y/Screen.height);
+
+        return Vector3.ClampMagnitude(new Vector3(panX, 0, panY), 1)*MousePanSpeed;
+    }
+
+    private Vector3 GetKeysMove()
+    {
+        var forwardMove = Input.GetKey(KeyCode.W) ? Vector3.forward : Vector3.zero;
+        var backMove = Input.GetKey(KeyCode.S) ? Vector3.back : Vector3.zero;
+        var leftMove = Input.GetKey(KeyCode.A) ? Vector3.left : Vector3.zero;
+        var rightMove = Input.GetKey(KeyCode.D) ? Vector3.right : Vector3.zero;
+
+        var keysMove = (forwardMove + backMove + leftMove + rightMove)*KeysPanSpeed;
+        return keysMove;
     }
 }
