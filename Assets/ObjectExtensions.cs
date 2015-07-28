a4d7b0ad02d7e29402085afcd40434f9f7166d45
Andreas Wilcox <unknownbum@gmail.com>
2015-07-28 20:47:56 +0200

    Added AsList-extension

--- Assets/ObjectExtensions.cs
+++ Assets/ObjectExtensions.cs
@@ -1,7 +1,15 @@
-﻿public static class ObjectExtensions
+﻿
+using System.Collections.Generic;
+
+public static class ObjectExtensions
 {
     public static Maybe<T> ToMaybe<T>(this T element)
     {
         return new Maybe<T>(element);
+    }
+
+    public static List<T> AsList<T>(this T element)
+    {
+        return new List<T>() { element };
     }
 }
